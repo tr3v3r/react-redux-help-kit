c9a59204c8e28eae919453d0cdcb62f77047f1d4
Aliaksei Astafyeu <lochost40@gmail.com>
2022-01-03 15:05:17 +0300

    release: bump version

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import {asyncReducers} from './redux-kit';
 import {useOnRequestSuccess, useRequestError, useRequestLoading} from './hooks';
 
 export {asyncReducers, useOnRequestSuccess, useRequestError, useRequestLoading};
+export {IError} from './types';
